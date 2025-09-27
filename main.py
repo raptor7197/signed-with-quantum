29f0456c5b29bf20606a0a3a0e4906cba12c078c
Gobi-Ananth <gobiananthx@gmail.com>
2025-09-27 12:51:41 +0530

    fix: stream image with caption in EXIF

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from PIL import Image
 import io
 import piexif
@@ -16,12 +16,16 @@ async def generate_caption(file: UploadFile = File(...)):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
     inputs = processor(images=image, return_tensors="pt")
-    out = model.generate(**inputs, min_length=10)
-    caption = processor.decode(out[0], skip_special_tokens=True)
-    exif_dict = piexif.load(image.info.get("exif", b""))
+    output_ids = model.generate(**inputs, min_length=10)
+    caption = processor.decode(output_ids[0], skip_special_tokens=True)
+    exif_dict = {"0th": {}, "Exif": {}, "GPS": {}, "1st": {}, "thumbnail": None}
     exif_dict["0th"][piexif.ImageIFD.ImageDescription] = caption.encode("utf-8")
     exif_bytes = piexif.dump(exif_dict)
     output = io.BytesIO()
     image.save(output, format="JPEG", exif=exif_bytes)
     output.seek(0)
-    return FileResponse(output, media_type="image/jpeg", filename="photo_with_caption.jpg")
+    return StreamingResponse(
+        output,
+        media_type="image/jpeg",
+        headers={"Content-Disposition": "inline; filename=photo_with_caption.jpg"}
+    )
